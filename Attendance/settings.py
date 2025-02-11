3852c649a49275a519dc5c0a802d8dec1604aa13
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2025-02-11 19:17:06 +0530

    Second commit

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -145,9 +145,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
